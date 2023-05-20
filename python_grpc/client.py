6083bac36786ddf4d9dc372b901b15ed92e5f680
Jared Myers <jaredjmyers@gmail.com>
2023-05-20 14:07:30 -0400

    add python grpc

---
+++ python_grpc/client.py
@@ -0,0 +1,93 @@
+from concurrent import futures
+
+import time
+import grpc
+from protos import users_pb2 as pb
+from protos import users_pb2_grpc as pbg
+
+from google.protobuf.json_format import MessageToDict
+
+URI = "localhost:50051"
+
+u1 = pb.User(id="10", name="tester", email="m.co", password="pw")
+u2 = pb.User(id="11", name="hello", email="p.co", password="123")
+u3 = pb.User(id="12", name="okay", email="n.co", password="00")
+test_users = [u1, u2, u3]
+
+
+def request_stream():
+    for user in test_users:
+        req = pb.CreateUserStreamRequest(user=user)
+        yield req
+        time.sleep(2)
+
+
+def run(URI):
+    with grpc.insecure_channel(URI) as channel:
+        stub = pbg.UsersStub(channel)
+
+        """
+        get_users_responses = stub.GetUserStream(pb.GetUserStreamRequest())
+        print(get_users_responses)
+        for response in get_users_responses:
+            print(response)
+        """
+
+        create_user_stream_response = stub.CreateUserStream(request_stream())
+
+        print(create_user_stream_response)
+
+
+
+
+        """
+        get_user_by_id_response = stub.GetUserById(users_pb2.GetUserByIdRequest(id="1"))
+
+        u3 = users_pb2.User(id="3", name="Funky", email="mail.as", password="pwwpww")
+        create_user_response = stub.CreateUser(users_pb2.CreateUserRequest(user=u3))
+
+        u3 = users_pb2.User(id="3", name="Crap", email="mail.as", password="pwwpww")
+        update_user_response = stub.UpdateUser(users_pb2.UpdateUserRequest(user=u3))
+
+        get_another_users_response = stub.GetUsers(users_pb2.GetUsersRequest())
+
+        delete_user_response = stub.DeleteUser(users_pb2.DeleteUserRequest(user=u3))
+
+        get_another2_users_response = stub.GetUsers(users_pb2.GetUsersRequest())
+        """
+
+    """
+
+    user = get_users_response.users[0]
+    test = MessageToDict(user)
+
+    print(test)
+    print(type(test))
+    print("-------------------")
+
+    print(get_user_by_id_response)
+    print(type(get_user_by_id_response))
+
+    print("create new users--")
+    print(create_user_response)
+    print(type(create_user_response))
+
+    print("--update users ---")
+    print(update_user_response)
+    print(type(update_user_response))
+
+    print("--check users again--")
+    print(get_another_users_response.users)
+    print(type(get_another_users_response.users))
+    print(type(get_another_users_response))
+
+    print("--deleted--")
+    print(delete_user_response)
+
+    print("---after--delete--")
+    print(get_another2_users_response)
+
+    """
+
+if __name__ == "__main__":
+    run(URI)
---
+++ python_grpc/server.py
@@ -0,0 +1,78 @@
+from concurrent import futures
+import logging
+import time
+
+import grpc
+from protos import users_pb2 as pb
+from protos import users_pb2_grpc as pbg
+
+
+class Users(pbg.UsersServicer):
+
+    u1 = pb.User(id="1", name="jon", email="email", password="password")
+    u2 = pb.User(id="2", name="Baxton", email="snail.co", password="pw")
+    u3 = pb.User(id="3", name="larry", email="email", password="sswo")
+    u4 = pb.User(id="4", name="Shawn", email="snail.co", password="piIkw")
+    u5 = pb.User(id="5", name="max", email="email", password="LKJ")
+    u6 = pb.User(id="6", name="mike", email="snail.co", password="JKL")
+    users = [u1, u2, u3, u4, u5, u6]
+
+    def GetUsers(self, request, context):
+        response = pb.GetUsersResponse(users=self.users)
+
+        return response
+
+    def GetUserById(self, request, context):
+        for user in self.users:
+            if user.id == request.id:
+                response = pb.GetUserByIdResponse(user=user)
+                break
+
+        return response
+
+    def CreateUser(self, request, context):
+        self.users.append(request.user)
+
+        return request
+
+    def UpdateUser(self, request, context):
+        id = int(request.user.id)
+        self.users[id-1] = request.user
+
+        return pb.UpdateUserResponse(id="3")
+
+    def DeleteUser(self, request, context):
+        id = int(request.user.id)
+        print(id)
+        print(type(id))
+        self.users = self.users[:(id-1)] + self.users[id:]
+
+        return pb.DeleteUserResponse(user=request.user)
+
+    def GetUserStream(self, request, context):
+        for user in self.users:
+            response = pb.GetUserStreamResponse(user=user)
+            yield response
+            time.sleep(2)
+
+    def CreateUserStream(self, request_iterator, context):
+        print("running creatUserStream")
+        count = 0
+        for request in request_iterator:
+            print(request)
+            count = count + 1
+
+        return pb.CreateUserStreamResponse(result=str(count))
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    pbg.add_UsersServicer_to_server(Users(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    serve()
